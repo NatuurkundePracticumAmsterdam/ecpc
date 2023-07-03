7b8294972f0984589b885e26ed31e21d51a88152
David Fokkema <davidfokkema@icloud.com>
2023-07-03 13:39:45 +0200

    Buildscript aangepast: alle pdfs naar svg

--- docs/figures/build.sh
+++ docs/figures/build.sh
@@ -1,9 +1,12 @@
 for file in *.tex; do
     pdflatex $file
-    pdf2svg ${file%.tex}.pdf ${file%.tex}.svg
 done
 
-# compile figure on pages
+for file in *.pdf; do
+    pdf2svg $file ${file%.pdf}.svg
+done
+
+# compile figures on extra pages
 pdf2svg p-n-overgang.pdf p-n-overgang_p2.svg 2
 pdf2svg layout-schematic.pdf layout-schematic_p2.svg 2
 pdf2svg layout-schematic.pdf layout-schematic_p3.svg 3
