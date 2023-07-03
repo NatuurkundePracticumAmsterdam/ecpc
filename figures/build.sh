2f144ff38ff7ab66bdc0ff812eb121e9c4b10a31
David Fokkema <davidfokkema@icloud.com>
2023-07-03 13:40:09 +0200

    Deployed 7b82949 with MkDocs version: 1.4.3

---
+++ figures/build.sh
@@ -0,0 +1,13 @@
+for file in *.tex; do
+    pdflatex $file
+done
+
+for file in *.pdf; do
+    pdf2svg $file ${file%.pdf}.svg
+done
+
+# compile figures on extra pages
+pdf2svg p-n-overgang.pdf p-n-overgang_p2.svg 2
+pdf2svg layout-schematic.pdf layout-schematic_p2.svg 2
+pdf2svg layout-schematic.pdf layout-schematic_p3.svg 3
+pdf2svg layout-schematic.pdf layout-schematic_p4.svg 4
