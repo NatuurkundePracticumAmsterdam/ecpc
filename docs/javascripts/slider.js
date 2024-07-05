5e8249338aae6617af524cce8257d1c18aff1355
Olivier Swaak <o.swaak@hotmail.com>
2024-07-05 17:18:49 +0200

    feat: dropdown menu for max range discrete slider
    https://github.com/NatuurkundePracticumAmsterdam/ecpc/issues/14#issuecomment-2211051809

--- docs/javascripts/slider.js
+++ docs/javascripts/slider.js
@@ -11,3 +11,17 @@ function updateMax() {
     document.getElementById('discrete_slider').max = document.getElementById('max_discrete').value;
     syncSliders('continuous_slider');
 }
+
+function syncSlidersBox(slider_id) {
+    let value = document.getElementById(slider_id).value;
+    if (slider_id == "continuous_slider_box") {
+        document.getElementById('discrete_slider_box').value = value * document.getElementById('discrete_slider_box').max/100;
+    } else {
+        document.getElementById('continuous_slider_box').value = value * 100/document.getElementById('discrete_slider_box').max;
+    }
+}
+
+function updateMaxBox() {
+    document.getElementById('discrete_slider_box').max = document.getElementById('max_discrete_box').value;
+    syncSlidersBox('continuous_slider_box');
+}
