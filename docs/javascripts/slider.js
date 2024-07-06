92a8302cb02518e435ce6c5c48c82381460aa5cb
Olivier Swaak <o.swaak@hotmail.com>
2024-07-06 16:22:34 +0200

    feat: ADC slider with labels showing current value
    https://github.com/NatuurkundePracticumAmsterdam/ecpc/issues/14#issuecomment-2211780809

--- docs/javascripts/slider.js
+++ docs/javascripts/slider.js
@@ -25,3 +25,51 @@ function updateMaxBox() {
     document.getElementById('discrete_slider_box').max = document.getElementById('max_discrete_box').value;
     syncSlidersBox('continuous_slider_box');
 }
+
+function syncSlidersLabels(slider_id) {
+    let value = document.getElementById(slider_id).value;
+    if (slider_id == "continuous_slider_labels") {
+        document.getElementById('discrete_slider_labels').value = value * document.getElementById('discrete_slider_labels').max/100;
+    } else {
+        document.getElementById('continuous_slider_labels').value = value * 100/document.getElementById('discrete_slider_labels').max;
+    }
+    document.getElementById('discrete_slider_labels').dispatchEvent(new Event('update_bubble', { bubbles: true }));
+    document.getElementById('continuous_slider_labels').dispatchEvent(new Event('update_bubble', { bubbles: true }));
+}
+
+function updateMaxLabels() {
+    document.getElementById('discrete_slider_labels').max = document.getElementById('max_discrete_labels').value;
+    syncSlidersLabels('continuous_slider_labels');
+}
+
+const allRanges = document.querySelectorAll(".range-wrap");
+allRanges.forEach(wrap => {
+  const range = wrap.querySelector(".range");
+  const bubble = wrap.querySelector(".bubble");
+
+  range.addEventListener("update_bubble", () => {
+    setBubble(range, bubble);
+  });
+  setBubble(range, bubble);
+});
+
+function setBubble(range, bubble) {
+    const val = range.value;
+    const min = range.min ? range.min : 0;
+    const max = range.max ? range.max : 100;
+    const newVal = Number(((val - min) * 100) / (max - min));
+  
+    if(bubble.classList.contains('bubble_below')) {
+      bubble.innerHTML = val;
+    } else {
+      bubble.innerHTML = parseFloat(String(val * 3.3 / 100)).toFixed(2);
+    }
+  
+    // Sorta magic numbers based on size of the native UI thumb
+    bubble.style.left = `calc(${newVal * 0.85}% + (${14.5 - newVal * 0.27 + 14}px)`;
+    if(bubble.classList.contains('bubble_below')) {
+      bubble.style.top = 2.3*range.offsetHeight + 'px';
+    } else {
+      bubble.style.top = -2*range.offsetHeight + 'px';
+    }
+  }
