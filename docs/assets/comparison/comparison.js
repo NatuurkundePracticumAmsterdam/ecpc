384b2801bb418fb618bf132c4f34827747dfcc78
Olivier Swaak <o.swaak@hotmail.com>
2024-07-11 14:33:40 +0200

    feat: highlights in both code blocks
    https://github.com/NatuurkundePracticumAmsterdam/ecpc/issues/5#issuecomment-2222306723

--- docs/assets/comparison/comparison.js
+++ docs/assets/comparison/comparison.js
@@ -4,6 +4,8 @@ function hoverFunction(id) {
     } else {
         target_id = "python_" + id.split("_")[1];
     }
-    let elem = document.getElementById(target_id);
+    let elem = document.getElementById(id);
     elem.classList.toggle('highlight');
+    let target_elem = document.getElementById(target_id);
+    target_elem.classList.toggle('highlight');
 }
