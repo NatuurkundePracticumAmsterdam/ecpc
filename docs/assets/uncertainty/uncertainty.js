193fbfe136f014131a97582bd66f5ff042638203
Olivier Swaak <o.swaak@hotmail.com>
2024-07-13 13:56:06 +0200

    fix: only get coordinates of click box
    https://github.com/NatuurkundePracticumAmsterdam/ecpc/issues/24#issuecomment-2226874297

--- docs/assets/uncertainty/uncertainty.js
+++ docs/assets/uncertainty/uncertainty.js
@@ -1,5 +1,6 @@
 function addPoint(e) {
-    var rect = e.target.getBoundingClientRect();
+    const click_box = document.getElementById("click_box");
+    var rect = click_box.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
     console.log(x + ", " + y);
@@ -7,9 +8,8 @@ function addPoint(e) {
     const datapoint = document.createElement("span");
     datapoint.classList.add("datapoint");
 
-    const parent = document.getElementById("click_box");
-    parent.appendChild(datapoint);
-    
+    click_box.appendChild(datapoint);
+
     datapoint.style.left = x + 'px';
     datapoint.style.top = y + 'px';
 }
