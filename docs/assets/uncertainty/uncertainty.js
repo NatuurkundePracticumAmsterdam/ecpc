8a7aa89f507f282cd016f11b581927d7a0c8cb2f
Olivier Swaak <o.swaak@hotmail.com>
2024-07-13 13:48:36 +0200

    feat: generate points on click
    https://github.com/NatuurkundePracticumAmsterdam/ecpc/issues/24#issuecomment-2226873190

--- docs/assets/uncertainty/uncertainty.js
+++ docs/assets/uncertainty/uncertainty.js
@@ -3,4 +3,13 @@ function addPoint(e) {
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
     console.log(x + ", " + y);
+
+    const datapoint = document.createElement("span");
+    datapoint.classList.add("datapoint");
+
+    const parent = document.getElementById("click_box");
+    parent.appendChild(datapoint);
+    
+    datapoint.style.left = x + 'px';
+    datapoint.style.top = y + 'px';
 }
