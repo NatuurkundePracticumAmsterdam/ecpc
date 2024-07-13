4f3b36a9a39ebc6c04c5c5f25e0531462805b431
Olivier Swaak <o.swaak@hotmail.com>
2024-07-13 15:31:32 +0200

    fix: datapoint location scales with screen size
    https://github.com/NatuurkundePracticumAmsterdam/ecpc/issues/24#issuecomment-2226904092

--- docs/assets/uncertainty/uncertainty.js
+++ docs/assets/uncertainty/uncertainty.js
@@ -1,8 +1,8 @@
 function addPoint(e) {
     const click_box = document.getElementById("click_box");
     var rect = click_box.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
+    var x = (e.clientX - rect.left - 6) / rect.width * 100;
+    var y = (e.clientY - rect.top - 6) / rect.width * 100;
     console.log(x + ", " + y);
 
     const datapoint = document.createElement("span");
@@ -10,8 +10,8 @@ function addPoint(e) {
 
     click_box.appendChild(datapoint);
 
-    datapoint.style.left = x - 6 + 'px';
-    datapoint.style.top = y - 6 + 'px';
+    datapoint.style.left = x + "%";
+    datapoint.style.top = y + "%";
 
     datapoint.style.zIndex = -1;
 }
