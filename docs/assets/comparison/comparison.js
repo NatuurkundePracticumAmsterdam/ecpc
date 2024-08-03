43f6858577dda94875045402cbd1347ac540167c
Olivier Swaak <o.swaak@hotmail.com>
2024-08-03 14:04:20 +0200

    feat: added internal comparisons
    #41
    Mockup 1
    https://github.com/NatuurkundePracticumAmsterdam/ecpc/pull/41#discussion_r1702698593

--- docs/assets/comparison/comparison.js
+++ docs/assets/comparison/comparison.js
@@ -1,12 +1,16 @@
 function hoverFunction(id) {
     if (id.includes("python")) {
         target_id = "shell_" + id.split("_")[1];
+    } else if (id.includes("shell")) {
+        target_id = "python_" + id.split("_")[1];
     } else if (id.includes("class")) {
         target_id = "function_" + id.split("_")[1];
     } else if (id.includes("function")) {
         target_id = "class_" + id.split("_")[1];
-    } else {
-        target_id = "python_" + id.split("_")[1];
+    } else if (id.includes("int")) {
+        target_id = "ext_" + id.split("_")[1];
+    } else if (id.includes("ext")) {
+        target_id = "int_" + id.split("_")[1];
     }
     let elem = document.getElementById(id);
     elem.classList.toggle('highlight');
