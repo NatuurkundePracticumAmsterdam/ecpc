efc658727573ccee7d0097ebb3ed05afc4c16d54
Olivier Swaak <o.swaak@hotmail.com>
2024-08-05 13:11:30 +0200

    style: add comments
    #41

--- docs/assets/comparison/comparison.js
+++ docs/assets/comparison/comparison.js
@@ -1,15 +1,19 @@
+// setup timer
 var end, start;
-
 start = new Date();
-hoverbool = false;
-var targetindex = 0;
 
+hoverbool = false; // interaction flag
+var targetindex = 0; // index for idle animation
+
+// targets for idle animation
 const target_list_function_class = ["function_keyword","function_name","function_args","function_instance","function_call","function_input"];
 const target_list_function_class_body = ["ext_instancemu4","ext_outputmu4"];
 
-const currentUrl = window.location.href;
+const currentUrl = window.location.href; // get url
 
+// compare two elements by highlighting them
 function hoverFunction(id) {
+    // find target element from hovered element
     if (id.includes("python")) {
         target_id = "shell_" + id.split("_")[1];
     } else if (id.includes("shell")) {
@@ -24,23 +28,29 @@ function hoverFunction(id) {
         target_id = "int_" + id.split("_")[1];
     }
 
+    // highlight compared elements
     let elem = document.getElementById(id);
     elem.classList.toggle('highlight_hover');
     
     let target_elem = document.getElementById(target_id);
     target_elem.classList.toggle('highlight');
     
+    // reset idle animation timeout and disable animation on interaction
     start = new Date();
     hoverbool = !hoverbool;
 }
 
 function comparisonIdleAnimation() {
+    // disable animation on interaction
     if (hoverbool) {
         start = new Date(); 
     }
+
+    // check if 3 s have passed since previous highlight to start new animation
     end = new Date();
     if (end.getTime() - start.getTime() >= 3000) {
         if (currentUrl.includes('classes')) {
+            // highlight next compared elements in list for 1 s
             hover_target = target_list_function_class[targetindex % 6];
             hoverFunction(hover_target);
             setTimeout(hoverFunction, 1000, hover_target);
@@ -54,4 +64,5 @@ function comparisonIdleAnimation() {
     }
 }
 
+// check if animation can play every 0.1 s
 setInterval(comparisonIdleAnimation, 100);
