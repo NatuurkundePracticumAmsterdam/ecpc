c722a9bee7f968473dd7d056724838f4c3f56503
Annelies Vlaar <66774020+AnneliesVlaar@users.noreply.github.com>
2024-10-17 17:07:06 +0200

    clean up cheatsheets
    remove from stand alone folder to docs

--- docs/assets/cheatsheets/cheatsheets.js
+++ docs/assets/cheatsheets/cheatsheets.js
@@ -31,7 +31,7 @@ if (currentUrl.includes("cheatsheets")) {
     cheatsheet_div.classList.add("cheatsheet");
     cheatsheet_div.classList.add("invisible");
     cheatsheet_div.id = "cheatsheet_div";
-    cheatsheet_div.innerHTML = '<iframe width="100%" height="100%" id="myHtml" src="../cheatsheets/cheatsheets"></iframe>'
+    cheatsheet_div.innerHTML = '<iframe width="100%" height="100%" id="myHtml" src="cheatsheets"></iframe>'
     // add icon and cheatsheet below eachother in grid
     wrapper_div.appendChild(icon_div);
     wrapper_div.appendChild(cheatsheet_div);
